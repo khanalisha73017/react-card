1156d72ac8b5a04cdf4f978d1e0e4856757a2943
khanalisha73017 <152811469+khanalisha73017@users.noreply.github.com>
2024-03-19 01:37:02 +0530

    Update AddProgressCard.jsx

--- demo-project/src/component/AddProgressCard.jsx
+++ demo-project/src/component/AddProgressCard.jsx
@@ -3,10 +3,11 @@ import styled from "styled-components";
 import { TbMessage } from "react-icons/tb";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import { Draggable } from "react-beautiful-dnd";
+import ProgressBar from "@ramonak/react-progress-bar";
 
 export const AddProgressCard = ({ title, No_of_message, id, index }) => {
   //   const { title } = Todos;
-  console.log(title,id,index, "tc");
+  console.log(title, id, index, "tc");
   return (
     <Draggable draggableId={`${id}`} key={id} index={index}>
       {(provided, snapshot) => (
@@ -17,7 +18,13 @@ export const AddProgressCard = ({ title, No_of_message, id, index }) => {
           isDragging={snapshot.isDragging}
         >
           <div className="main">
-            <input type="range" />
+            <ProgressBar
+              completed={100}
+              width="30%"
+              isLabelVisible={false}
+              animateOnRender={true}
+              bgColor="#e6c60d"
+            />
             <p>{title}</p>
             <div className="icon">
               <span>
@@ -35,8 +42,11 @@ export const AddProgressCard = ({ title, No_of_message, id, index }) => {
   );
 };
 const TODOCARD = styled.div`
+  display: block;
+  align-items: start;
+  justify-content: start;
   border-radius: 4px;
-  margin-top: 1rem;
+  margin-bottom: 1rem;
   background-color: #ffffff;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
@@ -49,6 +59,9 @@ const TODOCARD = styled.div`
 
     cursor: pointer;
     transition: 0.2s;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
   }
 
   .main:hover {
@@ -56,18 +69,12 @@ const TODOCARD = styled.div`
     transform: scale(1.03);
   }
 
-  input[type="range"] {
-    margin: 0px;
-    padding: 0px;
-  }
-
   p {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
     margin: 0px;
-    padding: 0px;
-    justify-items: start;
-    align-items: start;
+
+    padding-top: 4px;
   }
 
   .icon {
