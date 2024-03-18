dbcf81da14204de33ff39dd42122ef3d8066e5b5
khanalisha73017 <152811469+khanalisha73017@users.noreply.github.com>
2024-03-19 01:34:23 +0530

    Update TodoCard.jsx

--- demo-project/src/component/TodoCard.jsx
+++ demo-project/src/component/TodoCard.jsx
@@ -4,6 +4,8 @@ import { TbMessage } from "react-icons/tb";
 import { HiOutlineMenuAlt2 } from "react-icons/hi";
 import { Draggable } from "react-beautiful-dnd";
 
+import ProgressBar from "@ramonak/react-progress-bar";
+
 export const TodoCard = ({ id, title, No_of_message, index }) => {
   console.log(id, index, "id");
 
@@ -17,7 +19,15 @@ export const TodoCard = ({ id, title, No_of_message, index }) => {
           isDragging={snapshot.isDragging}
         >
           <div className="main">
-            <input type="range" />
+            <ProgressBar
+              completed={100}
+              width="30%"
+              height="20px"
+              isLabelVisible={false}
+              animateOnRender={true}
+              bgColor="#e6c60d"
+            />
+
             <p>{title}</p>
 
             <div className="icon">
@@ -37,8 +47,11 @@ export const TodoCard = ({ id, title, No_of_message, index }) => {
   );
 };
 const TODOCARD = styled.div`
+  display: block;
+  align-items: start;
+  justify-content: start;
   border-radius: 4px;
-  margin-top: 1rem;
+  margin-bottom: 1rem;
   background-color: #ffffff;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
     rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
@@ -52,6 +65,9 @@ const TODOCARD = styled.div`
 
     cursor: pointer;
     transition: 0.2s;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    padding-left: 10px;
   }
 
   .main:hover {
@@ -59,18 +75,12 @@ const TODOCARD = styled.div`
     transform: scale(1.03);
   }
 
-  input[type="range"] {
-    margin: 0px;
-    padding: 0px;
-  }
-
   p {
     display: grid;
     grid-template-columns: repeat(1, 1fr);
     margin: 0px;
     padding: 0px;
-    justify-items: start;
-    align-items: start;
+    padding-top: 4px;
   }
 
   .icon {
